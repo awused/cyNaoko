888b236a5fb3f6941847893583600b52c53be186
Falaina <rudd@csail.mit.edu>
2012-03-13 18:13:09 -0700

    Fix indentation

--- denshi.py
+++ denshi.py
@@ -1,7 +1,11 @@
 # Denshi - A prototype synchtube bot
-# Written in 2011 by falaina falaina@falaina.net
-# To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
-# You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+# Written in 2011 by Falaina falaina@falaina.net
+# To the extent possible under law, the author(s) have dedicated all
+# copyright and related and neighboring rights to this software to the
+# public domain worldwide. This software is distributed without any
+# warranty.  You should have received a copy of the CC0 Public Domain
+# Dedication along with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 import hashlib
 import itertools
