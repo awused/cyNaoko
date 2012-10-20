3a3e2ee96145c07110cd57e884b676e1f521860a
Suwako <Desuwaifu@gmail.com>
2012-10-20 11:23:40 +0000

    Added database journals to gitignore and improved the web server to the point where it might even work.

--- naoko/webserver.py
+++ naoko/webserver.py
@@ -20,36 +20,36 @@ class NaokoWebServer(object):
     def start(self):
         route("/")(self.render)
         if self.protocol == "fastcgi":
-            application = default_app()
-            WSGIServer(application, bindAddress=(self.host, int(self.port))).run()
+            from flup.server.fcgi import WSGIServer
+            WSGIServer(default_app(), bindAddress=(self.host, int(self.port))).run()
         elif self.protocol == "http":
             run(host=self.host, port=int(self.port))
 
 if __name__ == "__main__":
     # Standalone mode runs the webserver as a daemon
     import ConfigParser
-    import logging, sys, os
-    from lib.database import NaokoDB
-    from flup.server.fcgi import WSGIServer
+    import sys, os
     from lib.daemon.manager import manageDaemon
 
     config = ConfigParser.RawConfigParser()
     config.read("naoko.conf")
     dbfile = config.get("naoko", "db_file")
-    webserver_mode = config.get("naoko", "webserver_mode")
-    webserver_host = config.get("naoko", "webserver_host")
-    webserver_port = config.get("naoko", "webserver_port")
-    webserver_protocol = config.get("naoko", "webserver_protocol")
-    assert webserver_mode == "standalone", "Web server not set to standalone mode"
+    mode = config.get("naoko", "webserver_mode")
+    host = config.get("naoko", "webserver_host")
+    port = config.get("naoko", "webserver_port")
+    protocol = config.get("naoko", "webserver_protocol")
+    assert mode == "standalone", "Web server not set to standalone mode"
     assert dbfile and dbfile != ":memory:", "No database file"
 
-    def startServer(dbfile, host, port, protocol):
+    def startServer():
+        import logging
+        from lib.database import NaokoDB
         logging.basicConfig(format='%(name)-15s:%(levelname)-8s - %(message)s', stream=sys.__stderr__)
         server = NaokoWebServer(NaokoDB(dbfile), host, port, protocol)
         server.start()
     
     command = sys.argv[1] if len(sys.argv) > 1 else None
-    manageDaemon(startServer, command, sys.argv[0], "/tmp/naokoweb.pid", os.path.abspath(os.getcwd()), args=[dbfile, webserver_host, webserver_port, webserver_protocol])
+    manageDaemon(startServer, command, sys.argv[0], "/tmp/naokoweb.pid", os.path.abspath(os.getcwd()))
     
 else:
     import time
@@ -59,7 +59,10 @@ else:
             # Sleep until the NaokoDB is ready
             time.sleep(1)
 
-        naoko.logger.debug("Starting web server in integrated mode on %s:%s." % (naoko.webserver_host, naoko.webserver_port, webserver_protocol))
-        server = NaokoWebServer(naoko.dbclient, naoko.webserver_host, naoko.webserver_port)
+        # flup doesn't work from threads that aren't the main thread
+        assert naoko.webserver_protocol == "http", "Embedded web server only supports http mode."
+
+        naoko.logger.debug("Starting web server in embedded mode on %s:%s:%s." % (naoko.webserver_protocol, naoko.webserver_host, naoko.webserver_port))
+        server = NaokoWebServer(naoko.dbclient, naoko.webserver_host, naoko.webserver_port, naoko.webserver_protocol)
         server.start()
 
