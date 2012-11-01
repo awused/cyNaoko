4f88e17b4e10e315b7c62ea3f3da874a5a19b0ff
Suwako <Desuwaifu@gmail.com>
2012-11-01 05:50:31 +0000

    Well it sort of works. Ugly layout and minimal styling, will have to fix.

--- web/static/js/naoko.js
+++ web/static/js/naoko.js
@@ -1,12 +1,44 @@
 google.load('visualization', '1', {'packages':['corechart', 'annotatedtimeline']});
-google.setOnLoadCallback(function(){
+$('body').ready(function(){
 
+popularVideoTable = $('#popular_video_table tbody')
+for (i = 0; i < popularVideos.length; i++){
+    row = $('<tr><td class="video"></td><td class="freq"></td></tr>');
+    switch (popularVideos[i][0]){
+        case "yt":
+            link = "http://youtube.com/watch?v=" + popularVideos[i][1];
+            break;
+        case "vm":
+            link = "http://vimeo.com/" + popularVideos[i][1];
+            break;
+        case "sc":
+            // Special case. Will have to use soundcloud's api from python to find the track url. TODO later
+            link = "#"
+            break;
+        case "bt":
+            link = "http://blip.tv/posts/" + popularVideos[i][1];
+            break;
+        case "dm":
+            link = "http://www.dailymotion.com/video/" + popularVideos[i][1];
+            break;
+        default:
+            link = "#";
+    }
+    row.children('.video').append($("<a></a>", {
+            text : popularVideos[i][2],
+            href :link,
+        }));
+    row.children('.freq').text(popularVideos[i][4]);
+    popularVideoTable.append(row);  
+}});
+
+google.setOnLoadCallback(function(){
 var userVideoData = new google.visualization.DataTable();
 userVideoData.addColumn('string', 'Topping');
 userVideoData.addColumn('number', 'Slices');
 userVideoData.addRows(userVideoStats);
 var userVideoChart = new google.visualization.PieChart(document.getElementById('user_video_div'));
-userVideoChart.draw(userVideoData/*, {width: 800, height: 480}*/);
+userVideoChart.draw(userVideoData, {width: "100%", height: "100%"});
 
 
 var userChatData = new google.visualization.DataTable();
@@ -14,31 +46,32 @@ userChatData.addColumn('string', 'Topping');
 userChatData.addColumn('number', 'Slices');
 userChatData.addRows(userChatStats);
 var userChatChart = new google.visualization.PieChart(document.getElementById('user_chat_div'));
-userChatChart.draw(userChatData/*, {width: 800, height: 480}*/);
-/*
-var dat = new google.visualization.DataTable();
-dat.addColumn('datetime', 'Time'); 
-dat.addColumn('number', 'Short Moving average');
-dat.addColumn('number', 'Long Moving average');
-dat.addColumn('number', 'Number of Users');
+userChatChart.draw(userChatData, {width: "100%", height: "100%"});
+
+
+var averageUserData = new google.visualization.DataTable();
+averageUserData.addColumn('datetime', 'Time'); 
+averageUserData.addColumn('number', 'Short Moving average');
+averageUserData.addColumn('number', 'Long Moving average');
+averageUserData.addColumn('number', 'Number of Users');
 var smaspan = 12*7;
 var lmaspan = 4*7*30;
-for(i=0; i<info.length;i++) {
+for(i=0; i<averageUsers.length;i++) {
     var row;
-    info[i][0] = new Date(info[i][0]); 
-    row = [info[i][0], 0, 0, info[i][1]]
+    averageUsers[i][0] = new Date(averageUsers[i][0]); 
+    row = [averageUsers[i][0], 0, 0, averageUsers[i][1]]
     if(i > (smaspan-1)) {
-        var sum = info.slice(i-smaspan, i).reduce(function(prev, cur) {return prev + cur[1]}, 0);
-        console.log(sum);
+        var sum = averageUsers.slice(i-smaspan, i).reduce(function(prev, cur) {return prev + cur[1]}, 0);
+        //console.log(sum);
         row[1] = sum/smaspan;
     }
     if(i > (lmaspan-1)) {
-        var sum = info.slice(i-lmaspan, i).reduce(function(prev, cur) {return prev + cur[1]}, 0);
-        console.log(sum);
+        var sum = averageUsers.slice(i-lmaspan, i).reduce(function(prev, cur) {return prev + cur[1]}, 0);
+        //console.log(sum);
         row[2] = sum/lmaspan;
     }
-    dat.addRow(row);
+    averageUserData.addRow(row);
 }
-var annotatedtimeline = new google.visualization.AnnotatedTimeLine(document.getElementById('info_div'));
-annotatedtimeline.draw(dat, {width: 800, height : 800, 'displayAnnotations': true, zoomStartTime : new Date(2011, 8, 8), zoomEndTime : new Date(), colors:['black', 'green', 'cyan'], max: 50});*/
+var averageUserTimeline = new google.visualization.AnnotatedTimeLine(document.getElementById('average_user_div'));
+averageUserTimeline.draw(averageUserData, {width: "800px", height : "800px", 'displayAnnotations': true, /*zoomStartTime : new Date(2011, 8, 8), zoomEndTime : new Date(),*/ colors:['black', 'green', 'cyan'], max: 50});
 });
